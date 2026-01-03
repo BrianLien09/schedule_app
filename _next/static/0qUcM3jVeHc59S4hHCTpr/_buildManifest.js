10d5bcd47a6735a293c71ed7f59a1e5e409afe8f
Brianlien09 <brianlien09@gmail.com>
2026-01-03 17:36:11 +0800

    Updates

--- _next/static/0qUcM3jVeHc59S4hHCTpr/_buildManifest.js
+++ _next/static/0qUcM3jVeHc59S4hHCTpr/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/schedule_app//_next/:path+",
+        "destination": "/schedule_app/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
