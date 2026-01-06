c4ef2bdf49e33f6ca793f003ba5c19ff8d7b6ab1
Brianlien09 <brianlien09@gmail.com>
2026-01-06 15:37:37 +0800

    feat:新增GitHub Actions workflow

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
+// 判斷是否為生產環境 (只有在 build 的時候才是 true)
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/schedule_app", // TODO: Update this to your GitHub repository name for deployment
+  
+  // 只有在部署(Production)時才加上 basePath，本地開發(Local)時保持空白
+  basePath: isProd ? "/schedule_app" : "", 
+  
   images: {
     unoptimized: true,
   },
